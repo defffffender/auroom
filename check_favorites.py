0fb1b2a6947f53f87cfb24444c4b516c7418c331
defffffender <arslanbek.077@gmail.com>
2025-12-19 00:49:19 +0500

    Remove temporary check script

--- check_favorites.py
+++
@@ -1,6 +0,0 @@
-from catalog.models import FavoriteList
-from django.contrib.auth.models import User
-
-print('=== Все списки избранного ===')
-for fl in FavoriteList.objects.all().select_related('user'):
-    print(f'ID: {fl.id}, User: {fl.user.username}, Name: {fl.name}, is_default: {fl.is_default}')
